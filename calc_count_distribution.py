47a90f46f09e1a1392f7a75d2f92d4d6ab6ca66d
k-wakisaka <k.wakisaka@vaikobo.com>
2023-12-03 04:14:01 +0900

    first commit

--- calc_count_distribution.py
+++ calc_count_distribution.py
@@ -3,6 +3,7 @@ import os
 import glob
 import json
 import numpy as np
+from tqdm import tqdm
 from sklearn.metrics import mean_squared_error
 
 
@@ -39,56 +40,74 @@ def mean_iou(results, gt_seg_maps, num_classes, ignore_index=0):
     mask[ignore_index] = False
     return np.mean(iou[mask]), distribution, iou
 
+def dump(json_dict, classes, answer_count_list, pred_count, skip_frame, distribution_dict, prefix):
+    print(f'[{prefix}]video_path: {os.path.basename(json_dict["video_path"])}')
+    mse_log_string = ""
+    answer_count_list_string = "Answer:\t"
+    pred_count_list_string = "Pred:\t"
+    for target_class_label in classes:
+        target_count_list = [0, ] * len(answer_count_list[0]['count'])
+        for answer_count in answer_count_list:
+            if answer_count['label'] == target_class_label:
+                count_list = [1 if count else 0 for count in answer_count['count']]
+                total_count_list = sum(count_list)
+                count_list = [count / total_count_list for count in count_list]
+                for count_index, count in enumerate(count_list):
+                    target_count_list[count_index] += count
+        target_count_list = target_count_list[::skip_frame]
+        target_pred_count = pred_count[::skip_frame, :, :, classes.index(target_class_label)]
+        print(target_class_label, np.max(target_pred_count))
+        pred_count_list = np.sum(target_pred_count, axis=-1)
+        pred_count_list = np.sum(pred_count_list, axis=-1).tolist()
+        mse = mean_squared_error(target_count_list, pred_count_list)
+        distribution_dict[target_class_label].append(mse)
+        mse_log_string += f'{target_class_label}: {json_dict["answer"][target_class_label] if target_class_label in json_dict["answer"].keys() else 0 }[count(answer)]-{sum(pred_count_list):.2f}[count(pred)]-{mse:.4f}[mse], '
+        answer_count_list_string += f'{target_class_label}: {[f"{elem:.2f}" for elem in target_count_list]}, '
+        pred_count_list_string += f'{target_class_label}: {[f"{elem:.2f}" for elem in pred_count_list]}, '
 
-def calc_count_distribution_mse(json_dict_list, classes, skip_frame):
-    distribution_dict = {}
-    for key in classes:
-        distribution_dict[key] = []
-    for json_dict in json_dict_list:
-        answer_count_list = json_dict['answer']['detail']
-        pred_count = np.asarray(json_dict['cam']['array']).reshape(json_dict['cam']['shape'])
-        print(f'video_path: {os.path.basename(json_dict["video_path"])}')
-        mse_log_string = ""
-        answer_count_list_string = "Answer:\t"
-        pred_count_list_string = "Pred:\t"
-        for target_class_label in classes:
-            target_count_list = [0, ] * len(answer_count_list[0]['count'])
-            for answer_count in answer_count_list:
-                if answer_count['label'] == target_class_label:
-                    count_list = [1 if count else 0 for count in answer_count['count']]
-                    total_count_list = sum(count_list)
-                    count_list = [count / total_count_list for count in count_list]
-                    for count_index, count in enumerate(count_list):
-                        target_count_list[count_index] += count
-            target_count_list = target_count_list[::skip_frame]
-            target_pred_count = pred_count[::skip_frame, :, :, classes.index(target_class_label)]
-            print(target_class_label, np.max(target_pred_count))
-            pred_count_list = np.sum(target_pred_count, axis=-1)
-            pred_count_list = np.sum(pred_count_list, axis=-1).tolist()
-            mse = mean_squared_error(target_count_list, pred_count_list)
-            distribution_dict[target_class_label].append(mse)
-            mse_log_string += f'{target_class_label}: {json_dict["answer"][target_class_label] if target_class_label in json_dict["answer"].keys() else 0 }[count(answer)]-{sum(pred_count_list):.2f}[count(pred)]-{mse:.4f}[mse], '
-            answer_count_list_string += f'{target_class_label}: {[f"{elem:.2f}" for elem in target_count_list]}, '
-            pred_count_list_string += f'{target_class_label}: {[f"{elem:.2f}" for elem in pred_count_list]}, '
-
-        print(mse_log_string)
-        print(answer_count_list_string)
-        print(pred_count_list_string)
-        print()
+    print(mse_log_string)
+    print(answer_count_list_string)
+    print(pred_count_list_string)
+    print()
     mean_acc_list = [np.mean(distribution_dict[label]) for label in classes]
     print(f'CountDistributionMSE[all]:{np.mean(mean_acc_list):.4f}')
     for index, label in enumerate(classes):
         print(f'{label}: {mean_acc_list[index]:.4f}')
     print()
+    return mean_acc_list
 
+def calc_count_distribution_mse(json_dict_list, classes, skip_frame):
+    distribution_dict = {}
+    for key in classes:
+        distribution_dict[key] = []
+    cam_mse_list = []
+    grad_mse_list = []
+    for _ in json_dict_list[0]['grad_cam']:
+        grad_mse_list.append([])
+    for json_dict in json_dict_list:
+        answer_count_list = json_dict['answer']['detail']
+        pred_count = np.asarray(json_dict['cam']['array']).reshape(json_dict['cam']['shape'])
+        cam_mse = dump(json_dict, classes, answer_count_list, pred_count, skip_frame, distribution_dict, 'cam')
+        cam_mse_list.append(cam_mse)
+        for grad_index, grad_cam in enumerate(json_dict['grad_cam']):
+            pred_count = np.asarray(grad_cam['array']).reshape(grad_cam['shape'])
+            grad_mse = dump(json_dict, classes, answer_count_list, pred_count, skip_frame, distribution_dict, f'grad{grad_index:02d}')
+            grad_mse_list[grad_index].append(grad_mse)
+    for pred_index in range(len(cam_mse_list[0])):
+        cam_mse_pred = np.mean([cam_mse[pred_index] for cam_mse in cam_mse_list])
+        print(f'{classes[pred_index]}, {cam_mse_pred:.2f}[cam], ', end='')
+        for grad_index, grad_mse in enumerate(grad_mse_list):
+            grad_mse_pred = np.mean([grad_mse_pred[pred_index] for grad_mse_pred in grad_mse])
+            print(f'{grad_mse_pred:.2f}[grad{grad_index:02d}], ', end='')
+        print()
 def main(input_json_dir_path, input_classes_path, skip_frame):
     with open(input_classes_path, 'r') as f:
         classes = f.readlines()
     classes = tuple([label.strip() for label in classes])
 
-    json_path_list = sorted(glob.glob(os.path.join(input_json_dir_path, '*.json')))
+    json_path_list = sorted(glob.glob(os.path.join(input_json_dir_path, '*.json')))[:2]
     json_dict_list = []
-    for json_path in json_path_list:
+    for json_path in tqdm(json_path_list):
         with open(json_path, 'r') as f:
             json_dict = json.load(f)
             json_dict_list.append(json_dict)
