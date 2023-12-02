80ab9cd39db1600fe59630e7cda3432fe5e74f90
k-wakisaka <k.wakisaka@vaikobo.com>
2023-12-03 03:49:04 +0900

    first commit

--- draw.py
+++ draw.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import glob
 import json
+from tqdm import tqdm
 import imageio
 import numpy as np
 from PIL import Image
@@ -19,7 +20,26 @@ def create_gif(image_list, output_file_path, duration=5):
     image_list[0].save(output_file_path,
                            save_all=True, append_images=image_list[1:], optimize=False, duration=duration, loop=0)
 
-
+def save_gif(pred_frames, frames, classes, colors, json_dict, output_dir_path, prefix):
+    canvas_image_list = []
+    for frame_index, frame in enumerate(frames):
+        canvas_image = Image.fromarray(frame).convert('RGBA')
+        for target_class_index, target_class_label in enumerate(classes):
+            target_pred_image = pred_frames[frame_index, :, :, target_class_index]
+            target_pred_max_value = np.max(pred_frames[:, :, :, target_class_index])
+            target_pred_image = target_pred_image / target_pred_max_value if target_pred_max_value != 0. else target_pred_image
+            target_pred_image = np.clip(target_pred_image*255, 0, 255).astype(np.uint8)
+            draw_image = np.zeros((target_pred_image.shape[0], target_pred_image.shape[1], 4), dtype=np.uint8)
+            draw_image[:, :, :3] = colors[target_class_index]
+            draw_image[:, :, 3] = target_pred_image
+            canvas_image = Image.alpha_composite(canvas_image, Image.fromarray(draw_image))
+        canvas_image_list.append(canvas_image)
+    count_str = ""
+    for label_index, label in enumerate(classes):
+        count_str += f"{label}_{json_dict['count'][label_index]:.2f}({json_dict['answer'][label] if label in json_dict['answer'].keys() else 0})-"
+    count_str = count_str[:-1]
+    output_gif_path = os.path.join(output_dir_path, f'{os.path.splitext(os.path.basename(json_dict["video_path"]))[0]}-pred(ans)-{count_str}_{prefix}.gif')
+    create_gif(canvas_image_list, output_gif_path)
 def get_image(json_dict_elem, colors):
     vis_image = np.zeros(json_dict_elem['shape'] + [3, ], dtype=np.uint8)
     org_image = np.reshape(np.asarray(json_dict_elem['array']), vis_image.shape[:-1])
@@ -44,7 +64,7 @@ def main(input_json_dir_path, input_classes_path, output_dir_path):
 
     json_path_list = glob.glob(os.path.join(input_json_dir_path, '*.json'))
     json_dict_list = []
-    for json_path in json_path_list:
+    for json_path in tqdm(json_path_list):
         with open(json_path, 'r') as f:
             json_dict = json.load(f)
             json_dict_list.append(json_dict)
@@ -52,28 +72,13 @@ def main(input_json_dir_path, input_classes_path, output_dir_path):
         video = imageio.get_reader(json_dict['video_path'],  'ffmpeg')
         frames = [frame for frame in video][::json_dict['skip_frame']]
         pred_frames = np.asarray(json_dict['cam']['array']).reshape(json_dict['cam']['shape'])
-        canvas_image_list = []
-        for frame_index, frame in enumerate(frames):
-            canvas_image = Image.fromarray(frame).convert('RGBA')
-            for target_class_index, target_class_label in enumerate(classes):
-                target_pred_image = pred_frames[frame_index, :, :, target_class_index]
-                target_pred_max_value = np.max(pred_frames[:, :, :, target_class_index])
-                target_pred_image = target_pred_image / target_pred_max_value if target_pred_max_value != 0. else target_pred_image
-                target_pred_image = np.clip(target_pred_image*255, 0, 255).astype(np.uint8)
-                draw_image = np.zeros((target_pred_image.shape[0], target_pred_image.shape[1], 4), dtype=np.uint8)
-                draw_image[:, :, :3] = colors[target_class_index]
-                draw_image[:, :, 3] = target_pred_image
-                canvas_image = Image.alpha_composite(canvas_image, Image.fromarray(draw_image))
-            canvas_image_list.append(canvas_image)
-        count_str = ""
-        for label_index, label in enumerate(classes):
-            count_str += f"{label}_{json_dict['count'][label_index]:.2f}({json_dict['answer'][label] if label in json_dict['answer'].keys() else 0})-"
-        count_str = count_str[:-1]
-        output_gif_path = os.path.join(output_dir_path, f'{os.path.splitext(os.path.basename(json_dict["video_path"]))[0]}-pred(ans)-{count_str}.gif')
-        create_gif(canvas_image_list, output_gif_path)
+        save_gif(pred_frames, frames, classes, colors, json_dict, output_dir_path, 'cam')
 
+        frames = [np.zeros(frame.shape, dtype=np.uint8) for frame in video][::json_dict['skip_frame']]
 
-
+        for grad_index, grad_cam in enumerate(json_dict['grad_cam']):
+            pred_frames = np.asarray(grad_cam['array']).reshape(grad_cam['shape'])
+            save_gif(pred_frames, frames, classes, colors, json_dict, output_dir_path, f'{grad_index:02d}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='inference')
